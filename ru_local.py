4155d727f6d541b60a9394689acb03fb465d786c
leaty-ell <lisalimanova14@gmail.com>
2025-10-21 16:44:26 +0700

    Create ru_local.py

---
+++ ru_local.py
@@ -0,0 +1,29 @@
+PROGRAM_TITLE = "=== Шестиугольная мозаика ==="
+MOSAIC_READY = "Мозаика готова! Нажмите на окно для выхода."
+WINDOW_TITLE = "Шестиугольная мозаика"
+
+
+AVAILABLE_COLORS = "Доступные цвета:"
+INVALID_COLOR = "Недопустимый цвет. Попробуйте снова."
+CHOOSE_FIRST_COLOR = "Выберите первый цвет: "
+CHOOSE_SECOND_COLOR = "Выберите второй цвет: "
+
+PLACEMENT_CHOICE = "Выберите вариант размещения цветов (1-чередование, 2-градиент): "
+INVALID_CHOICE = "Неверный выбор! Введите 1 или 2."
+ALTERNATING = "alternating"
+GRADIENT = "gradient"
+
+HEXAGON_COUNT_PROMPT = "Введите количество шестиугольников в ряду (4-20): "
+INVALID_NUMBER = "Неверный ввод! Введите число."
+NUMBER_RANGE_ERROR = "Число должно быть от 4 до 20!"
+
+COLOR_NAMES = {
+    'красный': 'red',
+    'синий': 'blue', 
+    'зеленый': 'green',
+    'желтый': 'yellow',
+    'фиолетовый': 'purple',
+    'оранжевый': 'orange',
+    'розовый': 'pink',
+    'голубой': 'cyan'
+}
